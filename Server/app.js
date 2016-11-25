082dbad2187620f76bee7162c0035292f011b287
leesangyoon93 <libertyoon@naver.com>
2016-11-25 15:56:47 +0900

    asdf

--- Server/app.js
+++ Server/app.js
@@ -19,6 +19,8 @@ mongoose.connect('mongodb://localhost/iot', function(err) {
   else
     console.log("DB connected!");
 });
+
+require('./models/data');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var Data = mongoose.model('Data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var data = new Data();
+  data.save();
+  res.render('index', { title: 'IOT Server' });
+});
+
+router.post('/getData', function(req, res) {
+  Data.find().exec(function(err, data) {
+    if(err) return res.json({'result': 'fail'});
+    if(data) return res.json(data[0]);
+    else return res.json({'result': 'fail'});
+  })
+});
+
+router.post('/updateData', function(req, res){
+  Data.find().exec(function(err, data) {
+    if(err) return res.json({'result': 'fail'});
+    if(data) {
+      var d = data[0];
+      d.pos1 = req.body.pos1;
+      d.pos2 = req.body.pos2;
+      d.pos3 = req.body.pos3;
+      d.pos4 = req.body.pos4;
+      d.save();
+      return res.json({'result': 'success'})
+    }
+    else return res.json({'result': 'fail'});
+  })
 });
 
 module.exports = router;
