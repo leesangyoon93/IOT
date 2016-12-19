cb8bb8b6e9d4ec4ec5a131778a2d723cee820d60
leesangyoon93 <libertyoon@naver.com>
2016-12-19 22:56:53 +0900

    asdf

--- IOT/app/src/main/java/com/example/leesangyoon/iot/DetailInfo.java
+++ IOT/app/src/main/java/com/example/leesangyoon/iot/DetailInfo.java
@@ -28,8 +28,10 @@ import java.util.TimerTask;
 public class DetailInfo extends AppCompatActivity {
 
     ImageView car1, car2, car3, car4, arrow1, arrow2, arrow3, arrow4, full;
+    ImageView btn1, btn2, btn3, btn4;
     Timer mTimer = null;
     TimerTask task = null;
+    Boolean is1, is2, is3, is4;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,6 +53,14 @@ public class DetailInfo extends AppCompatActivity {
         arrow3 = (ImageView)findViewById(R.id.arrow3);
         arrow4 = (ImageView)findViewById(R.id.arrow4);
         full = (ImageView)findViewById(R.id.full);
+        btn1 = (ImageView)findViewById(R.id.temp);
+        btn2 = (ImageView)findViewById(R.id.temp2);
+        btn3 = (ImageView)findViewById(R.id.temp3);
+        btn4 = (ImageView)findViewById(R.id.temp4);
+        is1 = false;
+        is2 = false;
+        is3 = false;
+        is4 = false;
 
         if(task != null) {
             task.cancel();
@@ -68,6 +78,51 @@ public class DetailInfo extends AppCompatActivity {
         };
         mTimer = new Timer();
         mTimer.schedule(task, 1000, 3000);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    updateDataToServer(1, is1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                is1 = !is1;
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    updateDataToServer(2, is2);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                is2 = !is2;
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    updateDataToServer(3, is3);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                is3 = !is3;
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    updateDataToServer(4, is4);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                is4 = !is4;
+            }
+        });
 
     }
 
@@ -163,4 +218,38 @@ public class DetailInfo extends AppCompatActivity {
 
         volley.getInstance().addToRequestQueue(req);
     }
+
+    private void updateDataToServer(final int carNumber, final boolean state) throws Exception {
+
+        Map<String, String> postParam = new HashMap<String, String>();
+        postParam.put("carNumber", String.valueOf(carNumber));
+        postParam.put("state", String.valueOf(state));
+
+        String URL = "http://52.41.19.232/updateData";
+
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, URL, new JSONObject(postParam), new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if (response.getString("result").equals("success")) {
+
+                    } else {
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("development", "Error: " + error.getMessage());
+            }
+        });
+
+        // Adding request to request queue
+        volley.getInstance().addToRequestQueue(req);
+    }
+
 }
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -4,52 +4,68 @@ var router = express.Router();
 var Data = mongoose.model('Data');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'IOT Server' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'IOT Server'});
 });
 
-router.post('/getData', function(req, res) {
-  Data.find().exec(function(err, data) {
-    if(err) return res.json({'result': 'fail'});
-    if(data) {
-      console.log(data);
-      return res.json(data[0]);
-    }
-    else return res.json({'result': 'fail'});
-  })
+router.post('/getData', function (req, res) {
+    Data.find().exec(function (err, data) {
+        if (err) return res.json({'result': 'fail'});
+        if (data) {
+            console.log(data);
+            return res.json(data[0]);
+        }
+        else return res.json({'result': 'fail'});
+    })
 });
 
 router.post('/getDistance', function (req, res) {
-  var latitude1 = parseFloat(req.body.latitude);
-  var longtitude1 = parseFloat(req.body.longtitude);
-  var distance = calDistance(37.2811028, 127.0507571, latitude1, longtitude1);
-  return res.json({'result': parseInt(distance)});
+    var latitude1 = parseFloat(req.body.latitude);
+    var longtitude1 = parseFloat(req.body.longtitude);
+    var distance = calDistance(37.2811028, 127.0507571, latitude1, longtitude1);
+    return res.json({'result': parseInt(distance)});
 });
 
-function calDistance(lat1, lon1, lat2, lon2){
+router.post('/updateData', function (req, res) {
+    Data.find().exec(function (err, data) {
+        if (err) return res.json({'result': 'fail'});
+        if (data) {
+            carNum = req.body.carNumber;
+            state = req.body.state;
+            if (carNum == "1") data[0].pos1 = !state;
+            if (carNum == "2") data[0].pos2 = !state;
+            if (carNum == "3") data[0].pos3 = !state;
+            if (carNum == "4") data[0].pos4 = !state;
+        }
+        else return res.json({'result': 'fail'});
+    })
 
-  var theta, dist;
-  theta = lon1 - lon2;
-  dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1))
-      * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-  dist = Math.acos(dist);
-  dist = rad2deg(dist);
+})
 
-  dist = dist * 60 * 1.1515;
-  dist = dist * 1.609344;    // 단위 mile 에서 km 변환.
-  dist = dist * 1000.0;      // 단위  km 에서 m 로 변환
+function calDistance(lat1, lon1, lat2, lon2) {
 
-  return dist;
+    var theta, dist;
+    theta = lon1 - lon2;
+    dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1))
+        * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+    dist = Math.acos(dist);
+    dist = rad2deg(dist);
+
+    dist = dist * 60 * 1.1515;
+    dist = dist * 1.609344;    // 단위 mile 에서 km 변환.
+    dist = dist * 1000.0;      // 단위  km 에서 m 로 변환
+
+    return dist;
 }
 
 // 주어진 도(degree) 값을 라디언으로 변환
-function deg2rad(deg){
-  return parseFloat(deg * Math.PI / parseFloat(180));
+function deg2rad(deg) {
+    return parseFloat(deg * Math.PI / parseFloat(180));
 }
 
 // 주어진 라디언(radian) 값을 도(degree) 값으로 변환
-function rad2deg(rad){
-  return parseFloat(rad * parseFloat(180) / Math.PI);
+function rad2deg(rad) {
+    return parseFloat(rad * parseFloat(180) / Math.PI);
 }
 
 module.exports = router;
